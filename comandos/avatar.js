9903df942cc59bd9ef568a2df1daefd5fb801b36
Javier Almarza <javieralmarzabucarey@gmail.com>
2021-07-01 15:23:20 -0400

    comando avatar

---
+++ comandos/avatar.js
@@ -0,0 +1,32 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+	nombre: 'avatar',
+	descripcion: 'Muestra el avatar del usuario mencionado o el avatar propio',
+	ejecutar(mensaje, argumentos){
+		// Variables para el comando.
+		let mensaje_avatar = new MessageEmbed()
+			.setColor(0x66b3ff);
+			
+		let persona_mencionada = mensaje.mentions.users.first();
+		console.log(`[DEV] Se menciono a:  ${persona_mencionada}`);
+
+		// CASO 1: No mencionan a nadie, mostrar el avatar propio.
+		if(!persona_mencionada) {
+			persona_mencionada = mensaje.author;
+			console.log(`[DEV] Se menciono al autor:  ${persona_mencionada}`);
+		}
+		
+		// CASO 2: Se menciona a alguien, mostrar el avatar de la persona mencionda. 
+		
+		// Agregando la información al embed.
+		console.log(persona_mencionada.displayAvatarURL(dynamic=true));
+		console.log(persona_mencionada.avatarURL());
+
+		mensaje_avatar
+			.setImage(`${persona_mencionada.displayAvatarURL()}`)
+			.setFooter(`Avatar de ${persona_mencionada.username}`);
+
+		mensaje.channel.send(mensaje_avatar);
+	}
+};
