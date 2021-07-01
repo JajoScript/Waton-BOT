d3555be5fed541a190f5bab63cb64df0675cc1b5
Jose Gonzalez <49794011+GODfataliti@users.noreply.github.com>
2021-07-01 19:19:21 -0400

    Modificando funcion cumpleaños
    Estimado, intente calcular los dias faltantes, espero que le sirva
    
    Un abrazo

--- comandos/cumpleaños.js
+++ comandos/cumpleaños.js
@@ -16,17 +16,41 @@ module.exports = {
             if(!esquema){
                mensaje.channel.send("El usuario no esta registrado en la base de datos 😞");
             }else {
-               // Calculo de los dias restantes para el cumpleaños.
+                // PRIMERO: OBTENER FECHA ACTUAL Y LA DEL CUMPLEAÑOS
+                let fecha_now = new Date() // Fecha de hoy.
+                let cumple_db = esquema.nacimiento 
+                let cumple_date = new Date(cumple_db) //Fecha Cumple
 
-               let fecha_hoy = new Date() // Fecha de hoy. 
-               let cumpleaños = esquema.nacimiento // 23-03-2001
-               let fecha_cumpleaños = new Date(cumpleaños)
-               // let milisegundos = fecha_hoy - fecha_cumpleaños
-               // let dias_restantes = milisegundos * 
 
-               console.log(`HOY : ${fecha_hoy}`);
-               console.log(`NACIMIENTO : ${cumpleaños}`);
-               console.log(`FECHA CUM : ${fecha_cumpleaños}`);
+                // OBTENER LOS VALORES INDIVUDUALES
+                let dia = cumple_date.getDate()
+                let mes = cumple_date.getMonth()
+                let anho = fecha_now.getFullYear()
+                // SEGUNDO: VERIFICAR SI ES QUE EL MES ACTUAL ES MAYOR O MENOR AL MES DEL CUMPLEAÑOS
+                let diferentMeses = cumple_date.getMonth() - fecha_now.getMonth()
+                //console.log(diferentMeses)
+                //SI EL VALOR ES MENOR O IGUAL A 0: EL CUMPLEAÑOS YA PASO
+                if (diferentMeses<=0){
+                    console.log('Ya paso')
+                    //OBTENER EL DIA Y MES DEL CUMPLEAÑOS Y SUMARLE 1 AL AÑO ACTUAL.
+                    console.log('Dia: '+dia+ ' Mes: '+mes +' Ano: '+ anho)
+                    let countDown = new Date(anho+1,mes-1,dia).getTime()
+                    let now = fecha_now.getTime()
+                    let diferencia = countDown - now
+                    let dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+                    console.log('Faltan: '+ dias+' dias')
+
+                //SI EL VALOR ES MAYOR A 0 EL CUMPLEAÑOS ESTA POR VENIR.
+                }else if (diferentMeses>0){
+                    console.log('Por venir.')
+                    //OBTENER EL DIA Y MES DEL CUMPLEAÑOS Y USAR EL AÑO ACTUAL.
+                    console.log('Dia: '+dia+ ' Mes: '+mes +' Ano: '+ anho)
+                    let countDown = new Date(anho,mes-1,dia).getTime()
+                    let now = fecha_now.getTime()
+                    let diferencia = countDown - now
+                    let dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+                    console.log('Faltan: '+ dias+' dias')
+}
 
             }
          })
