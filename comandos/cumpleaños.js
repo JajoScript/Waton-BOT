cc6d5baf5e37c15d69d02217b7a0902fd6eb076d
Jose Gonzalez <49794011+GODfataliti@users.noreply.github.com>
2021-07-01 20:59:44 -0400

    Actualizacion Camel Case
    Estimado, favor de revisar
    
    Un abrazo.

--- comandos/cumpleaños.js
+++ comandos/cumpleaños.js
@@ -8,43 +8,43 @@ module.exports = {
 	nombre: "cumpleaños",
 	descripcion: "Responde cuantos dias faltan para el CUMpleaños del mencionado.",
 	ejecutar(mensaje, argumentos){
-      let persona_mencionada = mensaje.mentions.users.first();
-      let persona_id = persona_mencionada.id
+      let PersonaMenciona = mensaje.mentions.users.first();
+      let personaID = PersonaMenciona.id
 
-      EsquemaUsuario.findOne({ userID : persona_id})
+      EsquemaUsuario.findOne({ userID : personaID})
          .then((esquema) => {
             if(!esquema){
                mensaje.channel.send("El usuario no esta registrado en la base de datos 😞");
             }else {
                 // PRIMERO: OBTENER FECHA ACTUAL Y LA DEL CUMPLEAÑOS
-                let fecha_ahora = new Date() // Fecha de hoy.
-                let cumple_db = esquema.nacimiento 
-                let cumple_fecha = new Date(cumple_db) //Fecha Cumple
+                let FechaAhora = new Date() // Fecha de hoy.
+                let CumpleDB = esquema.nacimiento 
+                let CumpleFecha = new Date(CumpleDB) //Fecha Cumple
 
 
                 // OBTENER LOS VALORES INDIVUDUALES
-                let dia = cumple_fecha.getDate()
-                let mes = cumple_fecha.getMonth()
-                let ano = fecha_ahora.getFullYear()
-		let ahora = fecha_ahora.getTime()
+                let dia = CumpleFecha.getDate()
+                let mes = CumpleFecha.getMonth()
+                let ano = FechaAhora.getFullYear()
+		let ahora = FechaAhora.getTime()
                 // SEGUNDO: VERIFICAR SI ES QUE EL MES ACTUAL ES MAYOR O MENOR AL MES DEL CUMPLEAÑOS
-                let diferencia_Mes = cumple_fecha.getMonth() - fecha_ahora.getMonth()
+                let DiferenciaMes = CumpleFecha.getMonth() - FechaAhora.getMonth()
                 //console.log(diferentMeses)
                 //SI EL VALOR ES MENOR O IGUAL A 0: EL CUMPLEAÑOS YA PASO
-                if (diferencia_Mes<=0){
+                if (DiferenciaMes<=0){
                     //OBTENER EL DIA Y MES DEL CUMPLEAÑOS Y SUMARLE 1 AL AÑO ACTUAL.
                     console.log('Dia: '+dia+ ' Mes: '+mes +' Ano: '+ ano)
-                    let cuenta_regresiva = new Date(ano+1,mes-1,dia).getTime()
-                    let diferencia = cuenta_regresiva - ahora
+                    let CuentaRegresiva = new Date(ano+1,mes-1,dia).getTime()
+                    let diferencia = CuentaRegresiva - ahora
                     let dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
                     console.log('Faltan: '+ dias+' dias')
 
                 //SI EL VALOR ES MAYOR A 0 EL CUMPLEAÑOS ESTA POR VENIR.
-                }else if (diferencia_Mes>0){
+                }else if (DiferenciaMes>0){
                     //OBTENER EL DIA Y MES DEL CUMPLEAÑOS Y USAR EL AÑO ACTUAL.
                     console.log('Dia: '+dia+ ' Mes: '+mes +' Ano: '+ ano)
-                    let cuenta_regresiva = new Date(ano,mes-1,dia).getTime()
-                    let diferencia = cuenta_regresiva - ahora
+                    let CuentaRegresiva = new Date(ano,mes-1,dia).getTime()
+                    let diferencia = CuentaRegresiva - ahora
                     let dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
                     console.log('Faltan: '+ dias+' dias')
 		}
