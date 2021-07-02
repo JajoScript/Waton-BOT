20c9f53c8750d7700c01d2888110cf6769055250
Javier Almarza <javieralmarzabucarey@gmail.com>
2021-07-01 22:13:54 -0400

    mongodb uri + heroku

--- eventos/ready.js
+++ eventos/ready.js
@@ -20,7 +20,7 @@ module.exports = {
 		console.log(`[BOT] BOT ${cliente.user.tag} encendido!`);
 
 		// Conectando a la BD.
-		mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/bot_discord", {useNewUrlParser: true, useUnifiedTopology: true})
+		mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
 			.then(() => console.log("[BOT][DB] Base de datos conectada!"))
 			.catch((err) => console.log(err));
 	}
