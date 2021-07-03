ba8e1ede5af8ef487fbff0321ea0c6d8dd10236b
Jose Gonzalez <49794011+GODfataliti@users.noreply.github.com>
2021-07-02 22:54:24 -0400

    Actualizacion fecha
    Estimado, por lo visto me falto verificar el dia, pido las disculpas correspondientes...
    
    Saludos

--- comandos/cumpleaños.js
+++ comandos/cumpleaños.js
@@ -26,36 +26,42 @@ module.exports = {
 					mensaje.channel.send("El usuario no esta registrado en la base de datos 😞");
 				} else {
 					// PRIMERO: OBTENER FECHA ACTUAL Y LA DEL CUMPLEAÑOS
-					let FechaAhora = new Date(); // Fecha de hoy.
-					let CumpleDB = esquema.nacimiento;
-					let CumpleFecha = new Date(CumpleDB); //Fecha Cumple
+               let FechaAhora = new Date(); // Fecha de hoy.
+               let CumpleDB = esquema.nacimiento;
+               let CumpleFecha = new Date(CumpleDB); //Fecha Cumple
 
-					// OBTENER LOS VALORES INDIVUDUALES
-					let dia = CumpleFecha.getDate();
-					let mes = CumpleFecha.getMonth();
-					let año = FechaAhora.getFullYear();
-					let ahora = FechaAhora.getTime();
+               // OBTENER LOS VALORES INDIVUDUALES
+               let dia = CumpleFecha.getDate();
+               let mes = CumpleFecha.getMonth();
+               let año = FechaAhora.getFullYear();
+               let ahora = FechaAhora.getTime();
 
-					// SEGUNDO: VERIFICAR SI ES QUE EL MES ACTUAL ES MAYOR O MENOR AL MES DEL CUMPLEAÑOS
-					let DiferenciaMes = CumpleFecha.getMonth() - FechaAhora.getMonth();
+               // SEGUNDO: VERIFICAR SI ES QUE EL MES ACTUAL ES MAYOR O MENOR AL MES DEL CUMPLEAÑOS
+               let DiferenciaMes = CumpleFecha.getMonth() - FechaAhora.getMonth();
 
-					//SI EL VALOR ES MENOR O IGUAL A 0: EL CUMPLEAÑOS YA PASO
-					if (DiferenciaMes <= 0) {
-						//OBTENER EL DIA Y MES DEL CUMPLEAÑOS Y SUMARLE 1 AL AÑO ACTUAL.
-						let CuentaRegresiva = new Date(año + 1, mes, dia).getTime();
-						let diferencia = CuentaRegresiva - ahora;
-						var dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+               //SI EL VALOR ES MENOR O IGUAL A 0: EL CUMPLEAÑOS YA PASO
+               if (DiferenciaMes <= 0) { 
+                  //VERIFICAR SI EL DIA YA PASO
+                  if(dia>=FechaAhora.getDate()){
+                     var CuentaRegresiva = new Date(año, mes, dia).getTime();
 
-						//SI EL VALOR ES MAYOR A 0 EL CUMPLEAÑOS ESTA POR VENIR.
-						var edad = (FechaAhora.getFullYear() - CumpleFecha.getFullYear()) + 1;
+                  }else{
+                     //OBTENER EL DIA Y MES DEL CUMPLEAÑOS Y SUMARLE 1 AL AÑO ACTUAL.
+                     var CuentaRegresiva = new Date(año + 1, mes, dia).getTime();
+                  }
+                  let diferencia = CuentaRegresiva - ahora;
+                  var dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
 
-					} else if (DiferenciaMes > 0) {
-						//OBTENER EL DIA Y MES DEL CUMPLEAÑOS Y USAR EL AÑO ACTUAL.
-						let CuentaRegresiva = new Date(año, mes, dia).getTime();
-						let diferencia = CuentaRegresiva - ahora;
-						var dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+                  //SI EL VALOR ES MAYOR A 0 EL CUMPLEAÑOS ESTA POR VENIR.
+                  var edad = (FechaAhora.getFullYear() - CumpleFecha.getFullYear()) + 1;
 
-						var edad = (FechaAhora.getFullYear() - CumpleFecha.getFullYear());
+               } else if (DiferenciaMes > 0) {
+                  //OBTENER EL DIA Y MES DEL CUMPLEAÑOS Y USAR EL AÑO ACTUAL.
+                  let CuentaRegresiva = new Date(año, mes, dia).getTime();
+                  let diferencia = CuentaRegresiva - ahora;
+                  var dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+
+                  var edad = (FechaAhora.getFullYear() - CumpleFecha.getFullYear());
 					}
 					
 					// Mensaje personalizado.
